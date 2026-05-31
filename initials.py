309c0840fa9a7a637d808d032364ae6223482892
richforevercode <2425404058@live.gctu.edu.gh>
2026-05-31 15:24:37 +0000

    Finished work for Scripting Assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Prints stylized large block letters of my initials (AEO).
+# Assignment Number: 1
+#
+# Name: ANANE-FREMPONG EUGENE ODURO
+# STUDENT ID: 2425404058
+# Email: mrananefrempong@gmail.com
+# Grader: Carolyn
+#
+# On my honor, ANANE-FREMPONG EUGENE ODURO, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This main function prints the user's initials in a stylized block format.
+    print()
+    print("...AEO")
+    print()
+    print("...   AAAAA   ...... EEEEEEEEEE ......  OOOOOOOO  ....**...")
+    print("...  AAAAAAA  ...... EEEEEEEEEE ...... OOOOOOOOOOO ...**...")
+    print("... AA     AA ...... EE         ...... OO       OO ...  ...")
+    print("... AA     AA ...... EE         ...... OO       OO ...  ...")
+    print("... AAAAAAAAA ...... EEEEEEE    ...... OO       OO ...  ...")
+    print("... AAAAAAAAA ...... EEEEEEE    ...... OO       OO ...  ...")
+    print("... AA     AA ...... EE         ...... OO       OO ...  ...")
+    print("... AA     AA ...... EE         ...... OO       OO ...  ...")
+    print("... AA     AA ...... EEEEEEEEEE ...... OOOOOOOOOOO ..****.")
+    print("... AA     AA ...... EEEEEEEEEE ......  OOOOOOOO  ...****.")
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
